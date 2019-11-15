72cca3eb776cc68bdbb77e55f940624e51bb1aa9
Sebastian Park <SebPark03@gmail.com>
2019-11-14 20:02:50 -0500

    Retrained on nine Pokémon and added dolls.
    Also changed scripts to work with Tensorflow 2.0.0.

--- scripts/retrain.py
+++ scripts/retrain.py
@@ -139,7 +139,7 @@ def create_image_lists(image_dir, testing_percentage, validation_percentage):
     into training, testing, and validation sets within each label.
   """
   if not gfile.Exists(image_dir):
-    tf.compat.v1.compat.v1.logging.error("Image directory '" + image_dir + "' not found.")
+    tf.compat.v1.logging.error("Image directory '" + image_dir + "' not found.")
     return None
   result = collections.OrderedDict()
   sub_dirs = [
